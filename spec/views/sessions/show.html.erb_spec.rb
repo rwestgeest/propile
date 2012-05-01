4c7fe5b5692c48015656e94469341c76c95955ad
unknown <vp@vaio.(none)>
2012-05-01 16:05:28 +0200

    add presenter email addresses to session in new.html

--- spec/views/sessions/show.html.erb_spec.rb
+++ spec/views/sessions/show.html.erb_spec.rb
@@ -7,6 +7,6 @@ describe "sessions/show" do
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    #rendered.should include "bla" 
   end
 end
