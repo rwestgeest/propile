0bc6f616f133819c023e604ba4cca43dab74198e
unknown <vp@vaio.(none)>
2012-05-01 15:38:48 +0200

    add presenter email addresses to session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,5 @@
 class Session < ActiveRecord::Base
   has_and_belongs_to_many :presenters
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :first_presenter_email, :second_presenter_email 
+  attr_accessor :first_presenter_email, :second_presenter_email 
 end
