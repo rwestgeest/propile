b8ef7a5dc9eb7e54089260c24b6d8504db698686
root <root@dev.(none)>
2012-09-05 20:50:48 +0200

    calculat paf for a program

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -31,7 +31,8 @@ describe Program do
 
   describe "calculatePafForPresenter" do
     let(:program) { FactoryGirl.build :program  }
-    let(:vote) { FactoryGirl.build :vote  }
+    let(:vote1) { FactoryGirl.build :vote  }
+    let(:vote2) { FactoryGirl.build :vote  }
     context "when presenter has not voted" do
       it "paf should be 0 " do
         program.calculatePafForPresenter([]).should == 0
@@ -39,15 +40,46 @@ describe Program do
     end
     context "when presenter has voted for 1 session which is not scheduled in program" do
       it "paf should be 0 " do
-        program.calculatePafForPresenter([vote]).should == 0
+        program.calculatePafForPresenter([vote1]).should == 0
       end
     end
     context "when presenter has voted for 1 session which is scheduled in program" do
       it "paf should be 1 " do
         program_entry = FactoryGirl.build(:program_entry, :program => program)
-        program_entry.session = vote.session
+        program_entry.session = vote1.session
         program_entry.save
-        program.calculatePafForPresenter([vote]).should == 1
+        program.calculatePafForPresenter([vote1]).should == 1
+      end
+    end
+    context "when presenter has voted for 2 sessions which are scheduled in program" do
+      it "paf should be 2 " do
+        program_entry1 = FactoryGirl.build(:program_entry, :program => program)
+        program_entry1.session = vote1.session
+        program_entry1.save
+        program_entry2 = FactoryGirl.build(:program_entry, :program => program)
+        program_entry2.session = vote2.session
+        program_entry2.save
+        program.calculatePafForPresenter([vote1, vote2]).should == 2
+      end
+    end
+    context "when presenter has voted for 2 sessions of which 1 is scheduled in program" do
+      it "paf should be 1 " do
+        program_entry1 = FactoryGirl.build(:program_entry, :program => program)
+        program_entry1.session = vote1.session
+        program_entry1.save
+        program.calculatePafForPresenter([vote1, vote2]).should == 1
+      end
+    end
+    context "when presenter has voted for 2 sessions which are scheduled in program in same slot" do
+      it "paf should be 1 ", :broken => true do
+        program_entry1 = FactoryGirl.build(:program_entry, :program => program)
+        program_entry1.session = vote1.session
+        program_entry1.save
+        program_entry2 = FactoryGirl.build(:program_entry, :program => program)
+        program_entry2.session = vote2.session
+        program_entry2.slot = program_entry2.slot = vote1.session
+        program_entry2.save
+        program.calculatePafForPresenter([vote1, vote2]).should == 1
       end
     end
   end
