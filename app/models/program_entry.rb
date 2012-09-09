686d59b6a05566ee8396ddbad2305cbee4097f0d
root <root@dev.(none)>
2012-09-09 16:16:59 +0200

    improvements to display of program -colors

--- app/models/program_entry.rb
+++ app/models/program_entry.rb
@@ -4,4 +4,17 @@ class ProgramEntry < ActiveRecord::Base
 
   attr_accessible :slot, :track, :comment, :span_entire_row
   attr_accessible :session_id, :program_id
+  
+  def topic_class
+    topic = session ? session.topic.downcase : ""
+    topic_class = case  
+      when topic.include?("techn")  then "technology"
+      when topic.include?("customer") || topic.include?("planning")  then "customer"
+      when topic.include?("case") || topic.include?("intro")  then "cases"
+      when topic.include?("team") || topic.include?("individual")  then "team"
+      when topic.include?("process") || topic.include?("improv")  then "process"
+      else ""
+    end
+  end
+
 end
