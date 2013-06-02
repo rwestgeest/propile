cc3a0608ed92ae8c94933d5161fed73d4da74ece
Vera Peeters <root@dev.(none)>
2013-06-02 20:36:14 +0200

    #8 review: relation things to improve and score

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,8 +6,21 @@ class Review < ActiveRecord::Base
   attr_accessible :session_id
 
   validates :things_i_like, :presence => true
-  validates :things_to_improve, :presence => true
+  validates :things_to_improve, :presence => true, :unless => :max_score? 
   validates :presenter, :presence => true
   validates :session, :presence => true
+  validates :score, :presence => true
+  validates_numericality_of :score
 
+  validate :no_things_to_improve_when_max_score
+   
+  def no_things_to_improve_when_max_score
+    if max_score? and !things_to_improve.blank?
+      errors.add(:things_to_improve, "can't be filled in when the score is 10/10. ")
+    end
+  end
+
+  def max_score?
+    score == 10
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe Review do
+  let (:review) { FactoryGirl.create :review }
+
   describe "saving" do
     it "is possible" do
-      review = FactoryGirl.create :review
+      review
       Review.first.should == review
     end
   end
@@ -12,4 +14,19 @@ describe Review do
   it { should validate_presence_of(:things_to_improve) }
   it { should validate_presence_of(:presenter) }
   it { should validate_presence_of(:session) }
+  it { should validate_presence_of(:score) }
+  it { should validate_numericality_of(:score) }
+
+  describe "relation things to improve and score" do
+    it "things to improve has to be empty when score is 10" do
+      review.score = '10'
+      review.things_to_improve = 'something to improve'
+      review.should be_invalid
+    end
+    it "things to improve has to be filled when score is lower than 10" do
+      review.score = '9'
+      review.things_to_improve = ''
+      review.should be_invalid
+    end
+  end
 end
