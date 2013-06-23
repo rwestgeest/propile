e02788ef04c97678db8a7ab12733e18e24447bbd
Vera Peeters <root@dev.(none)>
2013-06-23 17:00:22 +0200

    #15 don't expire session after 2 minutes (was test config)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-#Propile::Application.config.session_store :cookie_store, key: '_propile_session', expire_after: 1.hours
-Propile::Application.config.session_store :cookie_store, key: '_propile_session', expire_after: 2.minutes
+Propile::Application.config.session_store :cookie_store, key: '_propile_session', expire_after: 1.hours
+#Propile::Application.config.session_store :cookie_store, key: '_propile_session', expire_after: 2.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
