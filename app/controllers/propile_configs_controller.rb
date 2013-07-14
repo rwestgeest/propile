d263514bb30d3c5122e2c070fd0d3df2512b5e34
Vera Peeters <root@dev.(none)>
2013-07-14 18:42:38 +0200

    #34 more statistics about reviews -- first try

--- app/controllers/propile_configs_controller.rb
+++ app/controllers/propile_configs_controller.rb
@@ -3,6 +3,13 @@ class PropileConfigsController < ApplicationController
     @propile_configs = PropileConfig.all
     @presenters = Presenter.all
     @number_of_reviews_by_presenters = Presenter.all.group_by {|p| p.reviews.size}.sort
+    @number_of_sessions_reviewed = Session.select {|s| !s.reviews.empty? }.size
+    @total_number_of_sessions = Session.all.size
+    @percentage_of_sessions_reviewed = (100 * @number_of_sessions_reviewed.to_f / @total_number_of_sessions).to_i
+    @total_number_of_reviews = Review.all.size
+    @total_number_of_reviewers = Presenter.all.select {|p| !p.reviews.empty?}.size 
+    @total_number_of_presenters = Presenter.all.size
+    @percentage_of_presenters_who_review = (100 * @total_number_of_reviewers.to_f / @total_number_of_presenters).to_i
 
     respond_to do |format|
       format.html # index.html.erb
