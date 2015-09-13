e8e5cb5e87297c91043c4e4062b8b7dad33bb8db
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2015-09-13 13:16:20 +0200

    Extract conference name from Notifications to Conference object

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,19 +1,19 @@
 # This class contains all conference-specific settings
 
 class Conference
-  NAME = "xXP Days"
+  NAME = "xyzXP Days"
   
-  AVAILABLE_DURATION = [ "x30 min", "x75 min", "x150 min" ]
+  AVAILABLE_DURATION = [ "xyz30 min", "xyz75 min", "xyz150 min" ]
 
-  AVAILABLE_SESSION_TYPE = [ "xhands on coding/design/architecture session", "xdiscovery session", "xexperiential learning session", "xshort experience report (30 min)"]
+  AVAILABLE_SESSION_TYPE = [ "xyzhands on coding/design/architecture session", "xyzdiscovery session", "xyzexperiential learning session", "xyzshort experience report (30 min)"]
 
-  AVAILABLE_TOPICS_AND_NAMES = { "xtechnology"=>"xTechnology and Technique",
-    "xcustomer"=>"xCustomer and Planning",
-    "xcases"=>"xIntro's and Cases",
-    "xteam"=>"xTeam and Individual",
-    "xprocess"=>"xProcess and Improvement",
-    "xother"=>"xOther"}
+  AVAILABLE_TOPICS_AND_NAMES = { "xyztechnology"=>"xyzTechnology and Technique",
+    "xyzcustomer"=>"xyzCustomer and Planning",
+    "xyzcases"=>"xyzIntro's and Cases",
+    "xyzteam"=>"xyzTeam and Individual",
+    "xyzprocess"=>"xyzProcess and Improvement",
+    "xyzother"=>"xyzOther"}
 
-  EMAIL_ADDRESS = "xsessions@xpday.net"
-  EMAIL_SUBJECT_PREFIX = "x[Propile XP Days 2015] "
+  EMAIL_ADDRESS = "xyzsessions@xpday.net"
+  EMAIL_SUBJECT_PREFIX = "xyz[Propile XP Days 2015] "
 end
