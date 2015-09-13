ab1b85357c29fb56f77da52ed96de25870532418
Pascal Van Cauwenberghe <pascal_github@nayima.be>
2015-09-13 12:14:03 +0200

    Extract email subject prefix into Conference model

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -5,10 +5,12 @@ class Conference
 
   AVAILABLE_SESSION_TYPE = [ "xhands on coding/design/architecture session", "xdiscovery session", "xexperiential learning session", "xshort experience report (30 min)"]
 
-   AVAILABLE_TOPICS_AND_NAMES = { "xtechnology"=>"xTechnology and Technique",
+  AVAILABLE_TOPICS_AND_NAMES = { "xtechnology"=>"xTechnology and Technique",
     "xcustomer"=>"xCustomer and Planning",
     "xcases"=>"xIntro's and Cases",
     "xteam"=>"xTeam and Individual",
     "xprocess"=>"xProcess and Improvement",
     "xother"=>"xOther"}
+
+  EMAIL_SUBJECT_PREFIX = "x[Propile XP Days 2015] "
 end
